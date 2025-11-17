0c3c0b34b9e02e0e8ae85d5bf793673c2ff4a5cc
Anyta Derkach Fem <annaderkac8@gmail.com>
2025-11-17 11:38:40 +0300

    Add files via upload

---
+++ kontrolnayaPresents/HelloApplication.java
@@ -0,0 +1,21 @@
+package com.psyche.nagrajdenie;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application
+{
+    @Override
+    public void start(Stage stage) throws IOException
+    {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 800);
+        stage.setTitle("🎀 Награждение Победителей Олимпиады");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
